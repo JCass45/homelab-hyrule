bd3a694b7a194d9a8137f23a78f6454d439226c4
Jack Cassidy <j.cassidy45@gmail.com>
2020-10-18 12:55:30 +0000

    add: Grafana, Telegraf and InfluxDB for all the metrics

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install docker && \
-sudo apt install docker-compose && \
-sudo usermod -aG docker $USER && \
-sudo systemctl enable docker
-
-echo "You should now reboot!"
